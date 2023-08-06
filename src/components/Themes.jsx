0f068d766acc3b0e09ae87528872c6ed75f5153f
Taiywo <54299107+Taiywo@users.noreply.github.com>
2023-08-06 20:33:12 +0100

    theme added

--- src/components/Themes.jsx
+++ src/components/Themes.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import ThemeItem from './ThemeItem'
 import { themes } from '../data'
 import { FaCog } from 'react-icons/fa'
-import {BsSun, BsMoon} from 'react-icons/bs'
+import { BsSun, BsMoon } from 'react-icons/bs'
 import './theme.css'
-import { document } from 'postcss'
+
 
 
 
@@ -15,42 +15,40 @@ const Themes = () => {
 
     const [color, setColor] = useState("red")
 
-    const changeColor = (color)=>{
+    const changeColor = (color) => {
         setColor(color)
     }
-
+   
     useEffect(() => {
-        if(error){
-            document.current.style.setProperty('--first-color', color);
-        }
-      
+        document.documentElement.style.setProperty('--first-color', color)
+          
     }, [color])
 
-    
 
 
-  return (
 
-    <div>
-        <div className={`${ showSwitcher ? "show-switcher": " "} style__switcher`}>
-            <div className="style__switcher-toggler" onClick={(()=>setShowSwitcher(!showSwitcher) )}  >
-                <FaCog/>
-            </div>
-            <div className="theme__toggle">
-                <BsMoon/>
-            </div>
-            <h3 className="style__switcher-title">Style Switcher</h3>
-            <div className="style__switcher-items">
-                {themes.map((theme,index)=>{
-                    return<ThemeItem key={index} {...theme} changeColor={changeColor}/>
-                })}
-            </div>
-            <div className="style__switcher-close" onClick={(()=>setShowSwitcher(!showSwitcher) )}>
-                &times;
+    return (
+
+        <div>
+            <div className={`${showSwitcher ? "show-switcher" : " "} style__switcher`}>
+                <div className="style__switcher-toggler" onClick={(() => setShowSwitcher(!showSwitcher))}  >
+                    <FaCog />
+                </div>
+                <div className="theme__toggle">
+                    <BsMoon />
+                </div>
+                <h3 className="style__switcher-title">Style Switcher</h3>
+                <div className="style__switcher-items">
+                    {themes.map((theme, index) => {
+                        return <ThemeItem key={index} {...theme} changeColor={changeColor} />
+                    })}
+                </div>
+                <div className="style__switcher-close" onClick={(() => setShowSwitcher(!showSwitcher))}>
+                    &times;
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Themes
